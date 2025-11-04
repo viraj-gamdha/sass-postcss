46f85bf7c15f6ada79fcec3eddb1e9dcb1f50bf4
Viraj Gamdha <tech.aplance@gmail.com>
2025-11-04 16:54:39 +0530

    Add Readme.md with few corrections

--- vite-postcss/src/components/button.tsx
+++ vite-postcss/src/components/button.tsx
@@ -1,5 +1,5 @@
 import styles from "./button.module.css";
 
 export default function Button({ children }: { children: React.ReactNode }) {
-  return <button className={styles.button}>{children}</button>;
+  return <button className={styles.base}>{children}</button>;
 }
